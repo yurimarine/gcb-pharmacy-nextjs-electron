9b3b703bd0ed490aea1e582684057c1bbf4ddf79
yurimarine <brandocalvento@gmail.com>
2025-12-03 15:00:28 +0800

    initial commit

---
+++ src/electron/main.js
@@ -0,0 +1,23 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  // Dev mode: load Next.js server
+  const isDev = !app.isPackaged;
+  if (isDev) {
+    win.loadURL("http://localhost:3000");
+  } else {
+    win.loadFile(path.join(__dirname, "../out/index.html"));
+  }
+}
+
+app.whenReady().then(createWindow);
